10d385f7477ddcb0fcb88add503e3a6b3802895d
André de Paula Terceiro <andre@terceiro.com.br>
2022-10-09 13:51:42 -0300

    Instalation script

---
+++ install.sh
@@ -0,0 +1,8 @@
+# First a global 'npm install'
+npm install
+
+# Then a npm install in the project folder
+cd ./jankenpo/
+npm install
+
+echo "Dependencies installed if we not have an error in 'npm install'. We only have 'npm install's in this file. To run the program please run ./jankenpo/bin/run"
