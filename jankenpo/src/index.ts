8078660e751ad20780aeab1a13f7a60b41c545af
André de Paula Terceiro <andre@terceiro.com.br>
2022-10-09 13:56:06 -0300

    Inserting semicolons in the main script

--- jankenpo/src/index.ts
+++ jankenpo/src/index.ts
@@ -1,20 +1,20 @@
-import {Command, flags} from '@oclif/command'
+import {Command, flags} from '@oclif/command';
 const Chalk = require('chalk');
-const inquirer = require("inquirer")
-const axios = require("axios")
+const inquirer = require("inquirer");
+const axios = require("axios");
 
 class Jankenpo extends Command {
-  static description = 'Jankenpo game'
+  static description = 'Jankenpo game';
 
   static flags = {
     version: flags.version({char: 'v'}),
     help: flags.help({char: 'h'}),
-  }
+  };
 
-  static args = [{name: 'file'}]
+  static args = [{name: 'file'}];
 
   async run() {
-      let choice:string = ""
+      let choice:string = "";
       
       await inquirer.prompt(
         {
@@ -27,28 +27,28 @@ class Jankenpo extends Command {
         choice = answer.choice;
       });
       
-      let computerChoice = ''
+      let computerChoice = '';
       await axios.get("https://187a7n5rz8.execute-api.us-west-2.amazonaws.com/default/jankenpo").then(
         function(response: any): any {
           computerChoice = response.data;
         }
       )
 
-      let winner = Chalk.yellow("draw")
+      let winner = Chalk.yellow("draw");
       computerChoice = this.translateChoice(computerChoice);
 
       if ((choice == "paper" && computerChoice == "rock") || (choice == "scissors" && computerChoice == "paper") || (choice == "rock" && computerChoice == "scissors")) {
-        winner = Chalk.green("player (you)")
+        winner = Chalk.green("player (you)");
       } else if ((choice == "paper" && computerChoice == "scissors") || (choice == "scissors" && computerChoice == "rock") || (choice == "rock" && computerChoice == "paper")) {
-        winner = Chalk.red("computer")
+        winner = Chalk.red("computer");
       }
 
-      console.log(choice + " (you)")
-      console.log(computerChoice + " (computer)")
+      console.log(choice + " (you)");
+      console.log(computerChoice + " (computer)");
 
-      console.log("You choosed: " + this.getColoredChoice(choice))
-      console.log("Computer choosed: " + this.getColoredChoice(computerChoice))
-      console.log("Winner was: " + winner)
+      console.log("You choosed: " + this.getColoredChoice(choice));
+      console.log("Computer choosed: " + this.getColoredChoice(computerChoice));
+      console.log("Winner was: " + winner);
   }
 
   getColoredChoice(choice: string): string {
